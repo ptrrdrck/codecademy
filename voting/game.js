09721528b93def45ec85f564581285abfc548ffd
ptrrdrck <56177908+ptrrdrck@users.noreply.github.com>
2022-11-28 15:48:47 -0800

    Voting setup

--- voting/game.js
+++ voting/game.js
@@ -34,10 +34,10 @@ guessButton.addEventListener('click', () => {
 
   // Display the winner
   if (humanIsWinner) {
-    guessButton.innerText = 'You Win!!!';
+    guessButton.innerText = 'You Win';
     guessButton.classList.toggle('winning-text')
   } else {
-    computerWinsDisplay.innerText = 'Computer Wins!!!';
+    computerWinsDisplay.innerText = 'Computer Wins';
   }
 
   // winnerDisplay.innerText = humanIsWinner ? 'You win!' : 'Computer wins!';
@@ -63,7 +63,7 @@ nextRoundButton.addEventListener('click', () => {
 
   // Reset the guess input box and the target number display:
   targetNumberDisplay.innerText = '?';
-  guessButton.innerText = 'Make a Guess';
+  guessButton.innerText = 'Vote "A"';
   humanGuessInput.value = '0';
   computerGuessDisplay.innerText = '?';
   computerWinsDisplay.innerText = '';
